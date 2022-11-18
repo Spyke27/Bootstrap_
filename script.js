0f19333c327e55415d0514a5a08c7189afcf0518
Spyke27 <rodrigoeufrasio2@gmail.com>
2022-11-18 11:43:25 -0300

    pronto

---
+++ script.js
@@ -0,0 +1,44 @@
+let nome = document.querySelector('#nome');
+let email = document.querySelector('#email');
+let btn = document.querySelector('.botao');
+let erroNome = document.querySelector('#erroNome');
+let erroEmail = document.querySelector('#erroEmail');
+
+let nomeOk = false;
+let emailOk = false;
+
+//Função para validar o nome do usuario
+function validaNome(){
+    if(nome.value.length < 3){
+        erroNome.innerHTML = 'Nome Inválido'
+    }
+    else {
+        erroNome.innerHTML = ''
+        nomeOk = true
+    }
+}
+
+
+//Função para validar o email
+function validaEmail(){
+    if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+        erroEmail.innerHTML = 'Email Inválido'
+    }
+    else{
+        erroEmail.innerHTML = ''
+        emailOk = true
+    }
+}
+
+btn.addEventListener("click", function() {
+    if(emailOk == true && nomeOk == true){
+        alert('Email enviado com sucesso!')
+    }
+    else{
+        alert('Preencha os campos corretamente!')
+    }
+});
+
+
+
+
